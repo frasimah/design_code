046896a749307b47d861093771c7c888bc83524a
alexandr rodionov <alexandr@alexandrs-MacBook-Pro.local>
2026-01-27 18:12:30 +0300

    Cleanup: Remove scrapers and debug files from repo

--- src/scraper/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Scraper modules"""
-
-from .base import BaseScraper
-from .products import VandersandenProductScraper
-from .browser_scraper import BrowserProductScraper, scrape_product_with_browser
-
-__all__ = ["BaseScraper", "VandersandenProductScraper", "BrowserProductScraper", "scrape_product_with_browser"]
